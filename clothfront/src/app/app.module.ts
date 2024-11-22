b386c4cef0da9655782fed5c1de680b26a21cdbf
Cam11lo <137002191+cam1lol@users.noreply.github.com>
2024-11-22 16:53:28 -0500

    Integracion de framework tailwind para css

--- clothfront/src/app/app.module.ts
+++ clothfront/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenunavComponent } from './menunav/menunav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenunavComponent
   ],
   imports: [
     BrowserModule,
