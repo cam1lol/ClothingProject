c29e86a633fb99b0cee8c93129c7f97ce7ec6df7
Cam11lo <137002191+cam1lol@users.noreply.github.com>
2024-11-26 12:41:16 -0500

    creacion de modulos para el menuslide

--- clothfront/src/app/app.module.ts
+++ clothfront/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenunavComponent } from './menu/menunav/menunav.component';
+import { DashboardComponent } from './menu/dashboard/dashboard.component';
+import { MediaComponent } from './menu/media/media.component';
+import { PagesComponent } from './menu/pages/pages.component';
+import { ProductsComponent } from './menu/products/products.component';
+import { SettingsComponent } from './menu/settings/settings.component';
+import { StatisticsComponent } from './menu/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenunavComponent,
+    DashboardComponent,
+    MediaComponent,
+    PagesComponent,
+    ProductsComponent,
+    SettingsComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
