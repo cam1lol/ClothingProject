a7337c0ecc785690c25a06cdcd845847a471e4dc
Cam11lo <137002191+cam1lol@users.noreply.github.com>
2024-11-26 19:12:26 -0500

    creacion de menu slidebar en html y css

--- clothfront/src/app/menu/menunav/menunav.component.ts
+++ clothfront/src/app/menu/menunav/menunav.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menunav',
   templateUrl: './menunav.component.html',
-  styleUrls: ['./menunav.component.css']
+  styleUrls: ['./menunav.component.css'],
 })
 export class MenunavComponent {
-  menuOpen = false;
+  // Propiedad para controlar la visibilidad de la barra lateral
+  isSidebarActive = false;
 
-  // Función para alternar el estado del menú
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  // Método para alternar el estado de la barra lateral
+  toggleSidebar(): void {
+    this.isSidebarActive = !this.isSidebarActive;
   }
 
+  toggleSubMenu(event: Event): void {
+    const parentLi = (event.target as HTMLElement).closest('li');
+    if (parentLi) {
+      parentLi.classList.toggle('active');
+      const siblings = Array.from(
+        parentLi.parentElement?.children || []
+      ).filter((child) => child !== parentLi);
+
+      // Remueve la clase "active" de los elementos hermanos
+      siblings.forEach((sibling) => sibling.classList.remove('active'));
+    }
+  }
 }
